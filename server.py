4b66505d31ebf599030d6392b87e265deb26d36b
Atubu88 <165842809+Atubu88@users.noreply.github.com>
2025-11-22 17:51:26 +0100

    Enhance audio extraction options for YouTube
    Updated ydl_opts to include extractor_args for YouTube.

--- server.py
+++ server.py
@@ -5,7 +5,18 @@ app = FastAPI()
 
 @app.get("/audio")
 def get_audio(url: str):
-    ydl_opts = {"format": "bestaudio", "noplaylist": True, "quiet": True}
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "noplaylist": True,
+        "quiet": True,
+        # 💚 обход защиты YouTube
+        "extractor_args": {
+            "youtube": {
+                "player_client": ["web"]
+            }
+        }
+    }
+
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=False)
         return {"audio_url": info["url"]}
